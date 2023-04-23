4a96b17fa759e5c0e2bbea5a58077caaa77cfa39
Priyanshu Patel <priyanshupatel007@gmail.com>
2023-04-23 13:19:41 +0530

    com1

--- src/App.js
+++ src/App.js
@@ -9,19 +9,19 @@ export default function App() {
      <Banner/>
 
     <h1 style={{fontSize:'2cm',textAlign:'center'}}>Exercises</h1>
-   
-     <Exercise img=<img src='https://d205bpvrqc9yn1.cloudfront.net/0001.gif' alt='Loading...'/>name="3/4 Sit-Up" des="Exercises keep you strong. 3/4 Sit-Up bup is one of the best
-      exercises to target your abs. It will help you improve your
-       mood and gain energy."  />
+      <div class="mid">
+        <Exercise img=<img src='https://d205bpvrqc9yn1.cloudfront.net/0001.gif' alt='Loading...'/>name="3/4 Sit-Up" des="Exercises keep you strong. 3/4 Sit-Up bup is one of the best
+          exercises to target your abs. It will help you improve your
+          mood and gain energy."  />
 
-        <Exercise img=<img src='https://d205bpvrqc9yn1.cloudfront.net/0002.gif'alt='Loading...'/>name="45° Side Bend" des="Exercises keep you strong. 45° Side Bend bup is one of the best
-        exercises to target your abs. It will help you improve your
-        mood and gain energy."/> 
+            <Exercise img=<img src='https://d205bpvrqc9yn1.cloudfront.net/0002.gif'alt='Loading...'/>name="45° Side Bend" des="Exercises keep you strong. 45° Side Bend bup is one of the best
+            exercises to target your abs. It will help you improve your
+            mood and gain energy."/> 
 
-        <Exercise img=<img src='https://d205bpvrqc9yn1.cloudfront.net/0003.gif'alt='Loading...'/>name="Air Bike" des="Exercises keep you strong. Air Bike bup is one of the best
-exercises to target your abs. It will help you improve your
-mood and gain energy."/> 
-
+            <Exercise img=<img src='https://d205bpvrqc9yn1.cloudfront.net/0003.gif'alt='Loading...'/>name="Air Bike" des="Exercises keep you strong. Air Bike bup is one of the best
+            exercises to target your abs. It will help you improve your
+            mood and gain energy."/> 
+      </div>
 
 
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -4,10 +4,11 @@ import logo from "../image/logo.png"
 export default function Header() {
   return (
     <div>
-      <img className='logo' src={logo}/>
+      
         <nav>
-       
+        <li><img className='logo' src={logo}/></li>
         <ul>
+        
         <li> <a href="app.js">Home</a></li>
         <li> <a href="#">Exercises</a></li>
         <li> <a href="#">Contact</a></li>
