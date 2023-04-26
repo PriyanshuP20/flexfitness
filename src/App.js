1fc9fc0503a5242268690d1eb1dd7b64c2de6056
Priyanshu Patel <priyanshupatel007@gmail.com>
2023-04-26 12:21:27 +0530

    a

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import React from 'react'
 import '../src/App.css';
 //import Home from './Home'
-import Banner from './components/Banner'
-import Exercise from './Exercise';
-export default function App() {
+
+import { Routes, Route, HashRouter } from "react-router-dom";
+import LandingPage from './components/landingPage';
+
+
+function App() {
   return (
-   <div>
-     <Banner/>
-
-    <h1 style={{fontSize:'2cm',textAlign:'center'}}>Exercises</h1>
-      <div class="mid">
-        <Exercise img=<img src='https://d205bpvrqc9yn1.cloudfront.net/0001.gif' alt='Loading...'/>name="3/4 Sit-Up" des="Exercises keep you strong. 3/4 Sit-Up bup is one of the best
-          exercises to target your abs. It will help you improve your
-          mood and gain energy."  />
-
-            <Exercise img=<img src='https://d205bpvrqc9yn1.cloudfront.net/0002.gif'alt='Loading...'/>name="45° Side Bend" des="Exercises keep you strong. 45° Side Bend bup is one of the best
-            exercises to target your abs. It will help you improve your
-            mood and gain energy."/> 
-
-            <Exercise img=<img src='https://d205bpvrqc9yn1.cloudfront.net/0003.gif'alt='Loading...'/>name="Air Bike" des="Exercises keep you strong. Air Bike bup is one of the best
-            exercises to target your abs. It will help you improve your
-            mood and gain energy."/> 
-      </div>
-
-
-
-
-
-
-    </div>
-     
-    
-  );
+    <>
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />}> 
+          <Route path="/" element={<LandingPage />} />
+          {/* <Route path="/services" element={<Search />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/Login" element={<Login />} /> */}
+        </Route>
+      </Routes>
+    </HashRouter>
+   
+    </>
+  )
 }
+
+export default App
+
---
+++ src/components/landingPage.js
@@ -0,0 +1,34 @@
+import Banner from '../components/Banner'
+import Exercise from '../Exercise';
+
+
+export default function LandingPage() {
+    return (
+     <div>
+       <Banner/>
+  
+      <h1 style={{fontSize:'2cm',textAlign:'center'}}>Exercises</h1>
+        <div class="mid">
+          <Exercise img=<img src='https://d205bpvrqc9yn1.cloudfront.net/0001.gif' alt='Loading...'/>name="3/4 Sit-Up" des="Exercises keep you strong. 3/4 Sit-Up bup is one of the best
+            exercises to target your abs. It will help you improve your
+            mood and gain energy."  />
+  
+              <Exercise img=<img src='https://d205bpvrqc9yn1.cloudfront.net/0002.gif'alt='Loading...'/>name="45° Side Bend" des="Exercises keep you strong. 45° Side Bend bup is one of the best
+              exercises to target your abs. It will help you improve your
+              mood and gain energy."/> 
+  
+              <Exercise img=<img src='https://d205bpvrqc9yn1.cloudfront.net/0003.gif'alt='Loading...'/>name="Air Bike" des="Exercises keep you strong. Air Bike bup is one of the best
+              exercises to target your abs. It will help you improve your
+              mood and gain energy."/> 
+        </div>
+  
+  
+  
+  
+  
+  
+      </div>
+       
+      
+    );
+  }
