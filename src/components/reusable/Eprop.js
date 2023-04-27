471e9d91fc85f1a12ade39186444ed789a7b5005
Priyanshu Patel <priyanshupatel007@gmail.com>
2023-04-27 19:06:54 +0530

    a

--- src/components/reusable/Eprop.js
+++ src/components/reusable/Eprop.js
@@ -1,12 +1,9 @@
 import React from 'react'
-// import '../style.css';
-// import 'C:/Users/priya/Desktop/flexfitness/src/pages/exercise.css'
 
-export default function Exe(props) {
+
+export default function Eprop(props) {
   return (
 
-    
-    <div>
       <div class="item item--medium" id="similarImage0">
                                             <div class="item__details" >
                                             <div className='imgg'>{props.img}</div>
@@ -14,11 +11,6 @@ export default function Exe(props) {
                                             <h2 className='des'>{props.des} </h2>
                                             </div>
       </div>
-        {/* <div className='card' >
-            <div className='imgg'>{props.img}</div>
-            <h3 className='title'>{props.name}</h3>
-            <h2 className='des'>{props.des} </h2>
-            </div> */}
-    </div>
+  
   )
 }
