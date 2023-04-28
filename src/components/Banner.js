d4fd8f6f3e9dde87d92834d2d6ac1f91ed8899eb
Saurav singh <91477208+Sauravsingh045@users.noreply.github.com>
2023-04-29 01:00:16 +0530

    Update Banner.js

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 import bg from "../image/images.jpg";
 import Button from "../components/reusable/button.js";
